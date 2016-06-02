a71ee35aa87a9fa71dee63da725fce487c9ad5cc
Llorch <skyakebeta@gmail.com>
2016-06-02 12:32:45 -0700

    Add style to clock

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,11 +1,14 @@
 var React = require('react');
+var Clock = require('Clock');
 
-var Countdown = (pros) => {
-	return(
-		<div>
-			<p>Countdown Page</p>
-		</div>
-	);
-};
+var Countdown = React.createClass({
+	render: function(){
+		return(
+			<div>
+				<Clock totalSeconds={129}/>
+			</div>
+		);
+	}
+});
 
 module.exports = Countdown;
