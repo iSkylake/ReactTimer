87eb2cd67dddc3f6d32c26535972dae3c6078c23
Llorch <skyakebeta@gmail.com>
2016-06-02 11:33:45 -0700

    Add Timer and Countdown page

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Countdown = (pros) => {
+	return(
+		<div>
+			<p>Countdown Page</p>
+		</div>
+	);
+};
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Timer = (pros) => {
+	return(
+		<div>
+			<p>Timer Page</p>
+		</div>
+	);
+};
+
+module.exports = Timer;
